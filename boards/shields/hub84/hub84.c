7c48789411f004e87b2f905b5fb5b5265b3ac63b
Remlin0202 <42442550@qq.com>
2026-02-20 19:11:34 +0800

    加入大写灯

---
+++ boards/shields/hub84/hub84.c
@@ -0,0 +1,42 @@
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/util.h>
+#include <zmk/hid_indicators.h>
+#include <zmk/events/hid_indicators_changed.h>
+
+#if IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
+
+#define LED_NODE DT_ALIAS(led_caps)
+
+#if DT_NODE_HAS_STATUS(LED_NODE, okay)
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+
+static int caps_led_init(void) {
+    if (!gpio_is_ready_dt(&led)) {
+        return -ENODEV;
+    }
+    return gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
+}
+
+SYS_INIT(caps_led_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
+
+static int caps_led_listener_cb(const zmk_event_t *eh) {
+    zmk_hid_indicators_t flags = zmk_hid_indicators_get_current_profile();
+    
+    // USB HID standard: Num Lock is bit 0, Caps Lock is bit 1, Scroll Lock is bit 2
+    if (flags & BIT(1)) { 
+        gpio_pin_set_dt(&led, 1);
+    } else {
+        gpio_pin_set_dt(&led, 0);
+    }
+    return 0;
+}
+
+ZMK_LISTENER(caps_led_listener, caps_led_listener_cb);
+ZMK_SUBSCRIPTION(caps_led_listener, zmk_hid_indicators_changed);
+
+#endif // DT_NODE_HAS_STATUS
+#endif // IS_ENABLED
